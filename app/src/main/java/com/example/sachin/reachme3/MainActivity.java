041c7205b08dafce543cac1a10c8190dadb15b6f
Sachin Ravanth <sachin.ravanth@gmail.com>
2018-09-18 14:23:10 +0530

    Added SharedPreferences

--- app/src/main/java/com/example/sachin/reachme3/MainActivity.java
+++ app/src/main/java/com/example/sachin/reachme3/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.sachin.reachme3;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -14,6 +17,7 @@ public class MainActivity extends AppCompatActivity {
     EditText editText;
     Button button,track,search;
     String phoneNumber;
+    SharedPreferences shared;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,12 +27,23 @@ public class MainActivity extends AppCompatActivity {
         button = (Button)findViewById(R.id.phoneButton);
         track = (Button)findViewById(R.id.track);
         search = (Button)findViewById(R.id.search);
+        shared = getSharedPreferences("ReachMe", Context.MODE_PRIVATE);
+        if (shared.contains("userKey")){
+            Log.d("paapu","Login:"+String.valueOf(shared.getBoolean("login",true)));
+            editText.setText(String.valueOf(shared.getString("userKey","NoUser")));
+            startActivity(new Intent(MainActivity.this,Register.class));
+            finish();
+        }
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 phoneNumber = editText.getText().toString();
                 DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference(getString(R.string.firebase_path)+"/"+getString(R.string.userDetails));
                 databaseReference.child(phoneNumber).child("phone").setValue(phoneNumber);
+                final SharedPreferences.Editor editor = shared.edit();
+                editor.putString("userKey",phoneNumber);
+                editor.putBoolean("login",true);
+                editor.commit();
             }
         });
         track.setOnClickListener(new View.OnClickListener() {
